d35ac3458ac323f947a9f41c41c7ec22b080927b
Praan26 <arpanap102@gmail.com>
2022-05-11 18:36:23 +0530

    [Arpan] Add: Mood Analyzer added

---
+++ MoodAnalyzer/MoodAnalyzer/AnanlyseMood.cs
@@ -0,0 +1,19 @@
+﻿namespace MoodAnalyzer
+{
+    public class AnanlyseMood
+    {
+        string message;
+        public AnanlyseMood(string message)
+        {
+            this .message = message;
+        }
+        public string MoodAnalyze()
+        {
+            if(message.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            return "Happy";
+        }
+    }
+}
---
+++ MoodAnalyzer/MoodAnalyzerTest/MoodTest.cs
@@ -0,0 +1,23 @@
+using MoodAnalyzer;
+using NUnit.Framework;
+
+namespace MoodAnalyzerTest
+{
+    public class MoodTest
+    {
+       [Test]
+       public void WhenAnalyzeMoodTest_ShouldGive_Output_As_Happy()
+        {
+            AnanlyseMood moodAnalyzer = new AnanlyseMood("I am in Happy Mood");
+            string actual=moodAnalyzer.MoodAnalyze();
+            Assert.AreEqual(actual,"Happy");
+        }
+        [Test]
+        public void WhenAnalyzeMoodTest_ShouldGive_Output_As_Sad()
+        {
+            AnanlyseMood moodAnalyzer = new AnanlyseMood("I am in Sad Mood");
+            string actual = moodAnalyzer.MoodAnalyze();
+            Assert.AreEqual(actual, "Sad");
+        }
+    }
+}
